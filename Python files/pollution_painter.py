91bca29fc83ac5fe7a0681946f4f04204e9e9333
Marin Visscher <m.r.visscher@science.leidenuniv.nl>
2023-08-23 12:56:32 +0200

    Added led_test and gitignore

--- Python files/pollution_painter.py
+++ Python files/pollution_painter.py
@@ -8,14 +8,6 @@ if user != 0:
 	print("Please run script as root")
 	sys.exit()
 	
-# LED strip configuration:
-LED_COUNT      = 219      # Number of LED pixels.
-LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
-LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
-LED_BRIGHTNESS = 255       # Set to 0 for darkest and 255 for brightest
-LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
-LED_CHANNEL    = 0
 
 def button(channel):
 	print("button pressed")
