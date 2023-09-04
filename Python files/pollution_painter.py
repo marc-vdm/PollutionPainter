22de4ecf0ca517c3ca55204ed98c5bf7c9114fe4
Marin Visscher <m.r.visscher@science.leidenuniv.nl>
2023-09-04 16:15:15 +0200

    Refactoring and integrating display handler

--- Python files/pollution_painter.py
+++ Python files/pollution_painter.py
@@ -1,37 +1,61 @@
 import os
-import sys
 import RPi.GPIO as GPIO
 
 #self-build dependencies
-import led_writer
-import pm_sensor
+import led_handler
+import sensor_handler
 import button_handler
+import display_handler
 
-#variables
-PM25 = 0
-sensor = pm_sensor.pm_sensor()
-leds = led_writer.led_writer()
+#Initiating the handlers
+display = display_handler.Display_Handler()
+sensor = sensor_handler.Sensor_Handler()
+leds = led_handler.Led_Handler()
 button = button_handler.button_handler()
 
 def main():
     button.press_callback = button_push
+    button.longpress_callback = button_longpress
+    display.STATUS.change("Ready")
+    
 
+#Callback function for a single button push
+#In this case darken the display and activity led
+#Start running the ledstrip, or vice versa
 def button_push():
-    print("button push")
     if leds.running:
         leds.stop()
+        display.restart()
+        pi_led_on()
     else:
         leds.start()
+        display.darken()
+        pi_led_off()
+
+#Callback function for a longpress
+#In this case darken the display and activity led
+#Or vice versa
+def button_longpress():
+    if display.darkened:
+        display.restart()
+        pi_led_on()
+    else:
+        display.darken()
+        pi_led_off()
+
+#Functions for turning the activity led of the pi on and off
+def pi_led_off():
+    os.system('echo 0 | sudo tee /sys/class/leds/led0/brightness > /dev/null')
+
+def pi_led_on():
+    os.system('echo 1 | sudo tee /sys/class/leds/led0/brightness > /dev/null')
 
 if __name__ == '__main__':
-    try:        
-        print('Starting up')
-        """
-        if os.getuid() != 0:
-            print("Please run script as root")
-            sys.exit()
-        """
+    try:
         main()
 
     except KeyboardInterrupt:
+        display.darken()
+        leds.stop()
+        pi_led_on()
         GPIO.cleanup()
